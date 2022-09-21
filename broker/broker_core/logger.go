da3b2820803f2a21224b20c38c122bd2b8fb980f
Patryk Zaniewski <kajgore@vp.pl>
2022-09-21 23:40:41 +0200

    rest and pubsub sandbox

--- broker/broker_core/logger.go
+++ broker/broker_core/logger.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"log"
 )
 
@@ -12,13 +11,21 @@ var (
 )
 
 func init() {
-	var buff bytes.Buffer
 
-	//InfoLogger = log.Default()
-	//InfoLogger.SetPrefix("INFO: ")
-	//
-	InfoLogger = log.New(&buff, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	InfoLogger = log.Default()
+	WarningLogger = log.Default()
+	ErrorLogger = log.Default()
 
+	InfoLogger.SetPrefix("INFO: ")
+	InfoLogger.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+	WarningLogger.SetPrefix("INFO: ")
+	WarningLogger.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+	ErrorLogger.SetPrefix("INFO: ")
+	ErrorLogger.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+	//InfoLogger = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
 	//WarningLogger = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
 	//ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 }

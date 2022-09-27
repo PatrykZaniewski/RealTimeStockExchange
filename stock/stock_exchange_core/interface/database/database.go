859a32f7446f42da72fb022c856974ecb4069468
Patryk Zaniewski <kajgore@vp.pl>
2022-09-27 21:40:44 +0200

    database transactions

--- stock/stock_exchange_core/interface/database/database.go
+++ stock/stock_exchange_core/interface/database/database.go
@@ -1,35 +1,49 @@
 package database
 
 import (
+	"cloud.google.com/go/firestore"
 	"context"
-	firebase "firebase.google.com/go"
 	"fmt"
-	"google.golang.org/api/iterator"
 	"log"
 )
 
 func DatabaseOperation() {
 	// Use a service account
 	ctx := context.Background()
-	conf := &firebase.Config{ProjectID: "citric-campaign-349210"}
-	app, err := firebase.NewApp(ctx, conf)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	client, err := app.Firestore(ctx)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	iter := client.Collection("order_book").Documents(ctx)
-	for {
-		doc, err := iter.Next()
-		if err == iterator.Done {
-			break
-		}
+	client, err := firestore.NewClient(ctx, "citric-campaign-349210")
+	ref := client.Collection("order_book").Doc("abc")
+	err = client.RunTransaction(ctx, func(ctx context.Context, tx *firestore.Transaction) error {
+		doc, err := tx.Get(ref) // tx.Get, NOT ref.Get!
 		if err != nil {
-			log.Fatalf("Failed to iterate: %v", err)
+			return err
 		}
-		fmt.Println(doc.Data())
+		pop, err := doc.DataAt("population")
+		tmp, err := ref.Get(ctx)
+		fmt.Println(tmp)
+		if err != nil {
+			return err
+		}
+		fmt.Print("1")
+		res := tx.Set(ref, map[string]interface{}{
+			"population": pop.(int64) + 1,
+		}, firestore.MergeAll)
+		fmt.Print("2")
+		return res
+	})
+	fmt.Println("3")
+	if err != nil {
+		// Handle any errors appropriately in this section.
+		log.Printf("An error has occurred: %s", err)
 	}
+	//for {
+	//	doc, err := iter.Next()
+	//	if err == iterator.Done {
+	//		break
+	//	}
+	//	if err != nil {
+	//		log.Fatalf("Failed to iterate: %v", err)
+	//	}
+	//	fmt.Println(doc.Data())
+	//}
 	defer client.Close()
 }
